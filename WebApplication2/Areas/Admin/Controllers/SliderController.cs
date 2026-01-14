a995b3a2b7513e6cf64d300291c6a8be52611574
Gülarə <gularabasova43-star>
2026-01-14 20:46:02 +0400

    Added Basket feature

---
+++ WebApplication2/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+           IEnumerable<Slider> sliders = await _context.Sliders.Where(m => !m.IsDeleted).ToListAsync();
+            return View(sliders);
+        }
+    }
+}
